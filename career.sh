61f31c1a462f12244736f5bb9b7ba455fac18dd2
pritamsingh2006 <pritamsingh26082006@gmail.com>
2025-11-28 22:20:39 +0530

    Add files via upload

---
+++ career.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Creating Career Development Folder Structure..."
+
+mkdir -p Career/Certifications
+mkdir -p Career/projects
+mkdir -p Career/Internships
+mkdir -p Career/Resumes
+mkdir -p Career/Documentation
+
+echo "Folders Created Successfully!"
+
+echo "System Uptime:"
+uptimes
